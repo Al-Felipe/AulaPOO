3d16fc5ede67c8324d477f5f3dbcf0aae5716451
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2023-03-28 21:52:43 -0300

    Add files via upload

---
+++ Pratica3_OO/src/Ingresso.java
@@ -0,0 +1,15 @@
+public class Ingresso {
+    float valor;
+
+    public Ingresso(float valor) {
+        this.valor = valor;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
---
+++ Pratica3_OO/src/Normal.java
@@ -0,0 +1,11 @@
+public class Normal extends Ingresso{
+
+    public Normal(float valor) {
+        super(valor);
+    }
+
+    public void valIngresso(){
+        System.out.println("Ingresso Normal");
+    }
+
+}
